0b8f79b0ec229c1f0fe401d6d5de93ce1deecc7b
kiike145 <enriquevilas9@gmail.com>
2023-05-04 10:11:40 +0200

    Se crea una plantilla general para la web

--- src/common/layout/GeneralLayout.jsx
+++ src/common/layout/GeneralLayout.jsx
@@ -4,11 +4,27 @@ import React from 'react';
 const GeneralLayout = ({ children }) => {
   return (
     <Grid container>
-      <Grid container sx={{ minHeight: '10vh', backgroundColor: 'primary.main' }}>
-        a
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ minHeight: '10vh', backgroundColor: 'primary.main', pr: '2rem', pl: '2rem' }}
+      >
+        <span>Foto</span>
+        <a href="/">Home</a>
+        <a href="/products">Productos</a>
+        <a href="/cart">Carro</a>
+
+        <a href="/login">Login</a>
+        <a href="/signup">Sign up</a>
       </Grid>
       {children}
-      <Grid container sx={{ minHeight: '10vh', backgroundColor: 'primary.main' }}>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: '10vh', backgroundColor: 'primary.main' }}
+      >
         a
       </Grid>
     </Grid>
