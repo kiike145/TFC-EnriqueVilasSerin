6a75886b9ebe77be6fa96c0d890d0f9321a8c5e3
kiike145 <enriquevilas9@gmail.com>
2023-05-23 17:12:48 +0200

    Se añade el controlador API de los productos

--- tfc-evs_spring/src/main/java/com/example/demo/apiController/ProductosControllerAPI.java
+++ tfc-evs_spring/src/main/java/com/example/demo/apiController/ProductosControllerAPI.java
@@ -1,2 +1,26 @@
-package com.example.demo.apiController;public class ProductosControllerAPI {
+package com.example.demo.apiController;
+
+import com.example.demo.model.Producto;
+import com.example.demo.service.IProductosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/store/prods")
+public class ProductosControllerAPI {
+
+    @Autowired
+    IProductosService productosServ;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Producto>> mostrarProductos() {
+        return new ResponseEntity<>(productosServ.obtenerProductosActivos() , HttpStatus.OK);
+    }
+
 }
