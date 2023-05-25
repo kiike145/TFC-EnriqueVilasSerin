ca7c2a5d80dda84b3791e7a5f7685d15a60499e6
kiike145 <enriquevilas9@gmail.com>
2023-05-25 12:42:37 +0200

    Metodo para calcular el precio total del pedido

--- tfc-evs_spring/src/main/java/com/example/demo/repository/DetallesPedidoRepository.java
+++ tfc-evs_spring/src/main/java/com/example/demo/repository/DetallesPedidoRepository.java
@@ -1,7 +1,21 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.DetallesPedido;
+import com.example.demo.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.lang.annotation.Native;
+import java.util.List;
 
 public interface DetallesPedidoRepository extends JpaRepository<DetallesPedido, Integer> {
+    List<DetallesPedido> findByPedido(Pedido p);
+
+    @Query(
+        value = "select sum(prod.precio * dp.cantidadProducto) as precioTotal from pedidos ped" +
+                "   inner join detallesPedido dp on ped.id = dp.idPedido" +
+                "   inner join productos prod on dp.idProducto = prod.id" +
+                "   where ped.id = ?",
+        nativeQuery = true)
+    Float getPrecioTotalByPedido(Integer idPedido);
 }
