bd573304a798ea4648f7dc9f61aa19fd373d5193
kiike145 <enriquevilas9@gmail.com>
2023-05-25 16:28:59 +0200

    Se incluye para realizar el Login

--- src/components/auth/pages/loginPage/LoginPage.jsx
+++ src/components/auth/pages/loginPage/LoginPage.jsx
@@ -1,22 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../layout/AuthLayout';
 import { Button, Grid, TextField } from '@mui/material';
 
 const LoginPage = () => {
+  const [error, setError] = useState([]); // Mostrarle los errores al usuario
+  const [formState, setFormState] = useState({
+    // Datos por defecto del formulario (vacio)
+    nombreusuario: '',
+    contrasena: '',
+  });
+
+  const handleChange = (event) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const logIn = () => {
+    fetch('http://localhost:8080/store/users/login/', {
+      method: 'POST',
+      body: JSON.stringify({
+        nombreusuario: formState.nombreusuario,
+        contrasena: formState.contrasena,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }).then((data) => {
+      if (data.status === 200) {
+        sessionStorage.setItem('isLogged', true);
+      }
+      if (data.status === 400) {
+        setError('User not found');
+      }
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setError('');
+    logIn();
+  };
+
   return (
     <AuthLayout title="Login">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField name="username" label="Nombre de usuario" color="secondary" fullWidth />
+            <TextField
+              name="nombreusuario"
+              label="Nombre de usuario"
+              color="secondary"
+              onChange={handleChange}
+              fullWidth
+            />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField name="password" label="Contraseña" color="secondary" fullWidth />
+            <TextField
+              name="contrasena"
+              label="Contraseña"
+              type="password"
+              color="secondary"
+              onChange={handleChange}
+              fullWidth
+            />
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <Button fullWidth variant="contained">
+            <Button fullWidth variant="contained" type="submit">
               <strong>Inicar sesión</strong>
             </Button>
           </Grid>
