061b49192a64addc3a6be627c0ea9682b2360d38
kiike145 <enriquevilas9@gmail.com>
2023-05-25 16:27:21 +0200

    Se permite el acceso desde la aplicacion de React

--- tfc-evs_spring/src/main/java/com/example/demo/apiController/UsuariosControllerAPI.java
+++ tfc-evs_spring/src/main/java/com/example/demo/apiController/UsuariosControllerAPI.java
@@ -9,6 +9,7 @@ import com.example.demo.service.IUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/store/users")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsuariosControllerAPI {
 
     @Autowired
@@ -42,7 +44,7 @@ public class UsuariosControllerAPI {
         if (usuarioServ.findUsuarioByNombreusuarioAndContrasena(user.getNombreusuario(), user.getContrasena())) {
 
             Usuario u = usuarioServ.getUsuarioByNombreusuarioAndContrasena(user.getNombreusuario(), user.getContrasena());
-
+            
             return new ResponseEntity<>(
                 detallesPedidoServ.obtenerDetallesPedidoByPedido(
                     pedidoServ.obtenerPedidoByUsuarioAndEstado(u , 1)
