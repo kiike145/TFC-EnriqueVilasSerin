8154fcc28801b73c860d35be000029225be0f6b6
kiike145 <enriquevilas9@gmail.com>
2023-05-26 14:45:20 +0200

    Se cambian los bloques de la barra de navegación

--- src/common/layout/GeneralLayout.jsx
+++ src/common/layout/GeneralLayout.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Grid, Toolbar } from '@mui/material';
+import { AppBar, Box, Grid, Toolbar } from '@mui/material';
 import React from 'react';
 
 const GeneralLayout = ({ children }) => {
@@ -16,16 +16,17 @@ const GeneralLayout = ({ children }) => {
           justifyContent="space-between"
           sx={{ minWidth: '100%', flexWrap: 'wrap', flexGrow: 0 }}
         >
-          <Grid item container xs={6} justifyContent="flex-start">
-            Foto&nbsp;
+          <Box>
+            Foto &nbsp;
             <a href="/">Home</a> &nbsp;
             <a href="/products">Productos</a> &nbsp;
             <a href="/cart">Carro</a>
-          </Grid>
-          <Grid item container xs={6} justifyContent="flex-end">
+          </Box>
+
+          <Box>
             <a href="/login">Login</a> &nbsp;
             <a href="/signup">Sign up</a>
-          </Grid>
+          </Box>
         </Grid>
       </Grid>
       <Grid container component="main" direction="column" sx={{ minHeight: '80vh', backgroundColor: '#F9F9F9' }}>
