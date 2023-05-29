63f385a37c39bec62cf0a08e2192aac76c1cd9be
kiike145 <enriquevilas9@gmail.com>
2023-05-29 16:10:25 +0200

    Componente para seleccionar las unidades

---
+++ src/components/store/pages/cartPage/SelecctorUnidadesProducto.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const SelecctorUnidadesProducto = ({ dp }) => {
+  const { producto } = dp;
+  const [unidadesSeleccionadas, setUnidadesSeleccionadas] = useState(dp.cantidadproducto);
+
+  const unidades = [];
+  for (let i = 1; i <= producto.cantidad; i++) {
+    unidades.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
+    );
+  }
+
+  const onHandleChange = (event) => {
+    const nuevaUnidadSeleccionada = +event.target.value;
+    setUnidadesSeleccionadas(nuevaUnidadSeleccionada);
+
+    fetch('http://localhost:8080/store/cart/', {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: dp.id,
+        cantidadproducto: nuevaUnidadSeleccionada,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  };
+
+  return (
+    <select value={unidadesSeleccionadas} onChange={onHandleChange}>
+      {unidades}
+    </select>
+  );
+};
+
+export default SelecctorUnidadesProducto;
