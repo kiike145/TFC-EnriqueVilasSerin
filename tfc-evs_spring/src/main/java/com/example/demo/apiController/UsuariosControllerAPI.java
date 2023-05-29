3f13ed207968b7e997344c00f5afb6ce1fcb55b6
kiike145 <enriquevilas9@gmail.com>
2023-05-29 16:12:42 +0200

    Se elimina endpoint

--- tfc-evs_spring/src/main/java/com/example/demo/apiController/UsuariosControllerAPI.java
+++ tfc-evs_spring/src/main/java/com/example/demo/apiController/UsuariosControllerAPI.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/store/users")
@@ -51,10 +50,5 @@ public class UsuariosControllerAPI {
         }
 
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-    }
-    
-    @PostMapping("/")
-    public void addProductoToPedidoByUsuario(@RequestBody DetallesPedido detallesPedido) {
-    	detallesPedidoServ.crearDetallesPedido(detallesPedido);
     }
 }
