8ff0001a49882e667028f7248415e5a5bffa9b5d
kiike145 <enriquevilas9@gmail.com>
2023-05-29 21:58:54 +0200

    Se añade el metodo para calcular precioTotal

--- tfc-evs_spring/src/main/java/com/example/demo/service/DetallesPedidoServiceImpl.java
+++ tfc-evs_spring/src/main/java/com/example/demo/service/DetallesPedidoServiceImpl.java
@@ -16,6 +16,10 @@ public class DetallesPedidoServiceImpl implements IDetallesPedido {
 
     @Autowired
     DetallesPedidoRepository detallesPedidoRepo;
+	@Autowired
+	IPedidoService pedidoServ;
+	@Autowired
+	IProductosService productoServ;
 
     @Override
     public void crearDetallesPedido(DetallesPedido detallesPedido) {
@@ -23,8 +27,43 @@ public class DetallesPedidoServiceImpl implements IDetallesPedido {
     	if (detallesPedido.getCantidadproducto() == null) {
     		detallesPedido.setCantidadproducto(1);
     	}
+    	
+    	Pedido pedidoAux = pedidoServ.obtenerPedidoById(detallesPedido.getPedido().getId());
+    	Float precioASumar = productoServ.obtenerProductoById(detallesPedido.getProducto().getId()).getPrecio() * detallesPedido.getCantidadproducto();
+    	
+    	if (pedidoAux.getPreciototal() == null) {
+    		pedidoAux.setPreciototal(precioASumar);
+    	} else {
+    		pedidoAux.setPreciototal(pedidoAux.getPreciototal() + precioASumar);
+    	}
+
+    	pedidoServ.guardarPedido(pedidoAux);
         detallesPedidoRepo.save(detallesPedido);
     }
+    
+	@Override
+	public void actulizarDetallesPedido(DetallesPedido dp) {
+		Optional<DetallesPedido> o = detallesPedidoRepo.findById(dp.getId());
+		
+		if (o.isPresent()) {
+			
+			Integer cantidadVieja = o.get().getCantidadproducto();
+			Integer cantidadNueva = dp.getCantidadproducto();
+			
+	    	Pedido pedidoAux = pedidoServ.obtenerPedidoById(o.get().getPedido().getId());
+	    	Producto prodAux = productoServ.obtenerProductoById(o.get().getProducto().getId());
+	    	
+	    	Float precioNuevo = (float) 0.0;
+	    	precioNuevo = prodAux.getPrecio() * cantidadNueva - prodAux.getPrecio() * cantidadVieja;
+			
+			o.get().setCantidadproducto(dp.getCantidadproducto());
+
+	    	pedidoAux.setPreciototal(pedidoAux.getPreciototal() + precioNuevo);
+            
+            pedidoServ.guardarPedido(pedidoAux);
+            detallesPedidoRepo.save(o.get());
+        }
+	}
 
     @Override
     public List<DetallesPedido> obtenerDetallesPedido() {
@@ -44,26 +83,33 @@ public class DetallesPedidoServiceImpl implements IDetallesPedido {
     }
 
     @Override
-    public List<DetallesPedido> obtenerDetallesPedidoByPedido(Pedido p) {
-        return detallesPedidoRepo.findByPedido(p);
+    public List<DetallesPedido> obtenerDetallesPedidoByIdPedido(Integer idPedido) {
+        return detallesPedidoRepo.findByPedido(pedidoServ.obtenerPedidoById(idPedido));
     }
 
 	@Override
-	public DetallesPedido obtenerDetallesPedidoByPedidoAndProducto(Pedido pedido, Producto producto) {
+	public DetallesPedido obtenerDetallesPedidoByPedidoAndProducto(DetallesPedido dp) {
+		
+		Pedido pedido = dp.getPedido();
+		Producto producto = dp.getProducto();
+		
 		return detallesPedidoRepo.findByPedidoAndProducto(pedido, producto);
 	}
 
 	@Override
 	public void eliminarDetallesPedidoByid(Integer id) {
-		detallesPedidoRepo.deleteById(id);
-	}
-
-	@Override
-	public void actulizarDetallesPedido(DetallesPedido dp) {
-		Optional<DetallesPedido> o = detallesPedidoRepo.findById(dp.getId());
+		
+		Optional<DetallesPedido> o = detallesPedidoRepo.findById(id);
+		
 		if (o.isPresent()) {
-            o.get().setCantidadproducto(dp.getCantidadproducto());
-            detallesPedidoRepo.save(o.get());
+			
+			Pedido pedidoAux = pedidoServ.obtenerPedidoById(o.get().getPedido().getId());
+	    	Float precioARestar = productoServ.obtenerProductoById(o.get().getProducto().getId()).getPrecio() * o.get().getCantidadproducto();
+	    	
+	    	pedidoAux.setPreciototal(pedidoAux.getPreciototal() - precioARestar);
+	    	pedidoServ.guardarPedido(pedidoAux);
         }
+		
+		detallesPedidoRepo.deleteById(id);
 	}
 }
