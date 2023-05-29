ac87af671fc6755a37db4a4d0224c490aa901e57
kiike145 <enriquevilas9@gmail.com>
2023-05-29 08:16:51 +0200

    Se crea funcion para asociar producto al pedido

--- src/components/store/pages/productPage/ProductListItem.jsx
+++ src/components/store/pages/productPage/ProductListItem.jsx
@@ -1,17 +1,28 @@
 import { Box, Button, Grid } from '@mui/material';
 import React from 'react';
 
-import './products.css';
-
 const ProductListItem = ({ product = {} }) => {
+  const handleOnClick = async () => {
+    await fetch('http://localhost:8080/store/users/', {
+      method: 'POST',
+      body: JSON.stringify({
+        producto: { id: product.id },
+        pedido: { id: sessionStorage.getItem('idPedido') },
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  };
+
   return (
-    <Box sx={{ m: 2, border: '1px solid black', p: 1, maxWidth: '14vw', bgcolor: '' }}>
-      <img src={product.imagen} />
+    <Box sx={{ m: 2, border: '1px solid black', p: 1, maxWidth: '14vw' }}>
+      <img src={product.imagen} className="imagenProducto" />
       <Grid container justifyContent="space-between">
         <span className="nombreProducto">{product.nombre}</span> <span>{product.precio}€</span>
       </Grid>
-      <Button fullWidth variant="contained" type="submit">
-        Ver detalles
+      <Button fullWidth variant="contained" onClick={handleOnClick}>
+        Añadir
       </Button>
     </Box>
   );
