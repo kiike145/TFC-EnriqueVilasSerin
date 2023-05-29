38856b05a5c5a571282d93f9bb51eac73024403c
kiike145 <enriquevilas9@gmail.com>
2023-05-29 13:25:57 +0200

    Se genera la lista de productos por pedido

--- src/components/store/pages/cartPage/CartPage.jsx
+++ src/components/store/pages/cartPage/CartPage.jsx
@@ -1,21 +1,43 @@
-import { Grid, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import GeneralLayout from '../../../../common/layout/GeneralLayout';
+import ListaProductosCarro from './ListaProductosCarro';
 
 export const CartPage = () => {
+  const [productos, setProductos] = useState([]);
+  const isLogged = sessionStorage.getItem('isLogged');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/store/cart/' + sessionStorage.getItem('idPedido'))
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status !== 400) {
+          setProductos(data);
+        }
+      });
+  }, []);
+
   return (
     <GeneralLayout>
-      <Grid
-        container
-        alignItems="center"
-        justifyContent="center"
-        direction="column"
-        spacing={0}
-        sx={{ minHeight: '80vh', backgroundColor: '#F9F9F9', padding: 4 }}
-      >
-        <Typography variant="h1" sx={{ mb: 1 }}>
+      <Grid container direction="column" spacing={0} sx={{ minHeight: '80vh', backgroundColor: '#F9F9F9', paddingLeft: 3, paddingRight: 3 }}>
+        <Typography variant="h5" sx={{ padding: 2 }}>
           Carrito
         </Typography>
+
+        <Grid container justifyContent="space-around">
+          {isLogged === null ? <p className="infoMsg">No hay ningun artículo</p> : <ListaProductosCarro productos={productos} />}
+
+          {isLogged === null ? (
+            ''
+          ) : (
+            <Grid container width="10vw" direction="column">
+              Total: €
+              <Box>
+                <Button variant="contained">Tramitar pedido</Button>
+              </Box>
+            </Grid>
+          )}
+        </Grid>
       </Grid>
     </GeneralLayout>
   );
