99857dc9a8125988389bd6eefb881af5576defc6
kiike145 <enriquevilas9@gmail.com>
2023-06-04 19:09:21 +0200

    Se incluye el banner y los estilos

--- src/components/store/pages/homePage/HomePage.jsx
+++ src/components/store/pages/homePage/HomePage.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Grid, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import GeneralLayout from '../../../../common/layout/GeneralLayout';
 import { useEffect } from 'react';
 import Article from './Article';
+
+import banner from '../../../../assert/banner.jpeg';
 
 export const HomePage = () => {
   const [products, setProducts] = useState([]);
@@ -15,11 +17,20 @@ export const HomePage = () => {
 
   return (
     <GeneralLayout>
-      <Typography variant="h1">Hola!</Typography>
-      <Grid container direction="row" wrap="wrap" justifyContent="space-around">
-        <Grid>banner</Grid>
+      <Grid container direction="column" justifyContent="center" alignItems="center" spacing={0} sx={{ minHeight: '80vh', backgroundColor: '#F9F9F9', paddingLeft: 3, paddingRight: 3 }}>
+        <img src={banner} alt="banner del la pagina principal" className="banner" />
+        <Typography variant="h1" sx={{ fontSize: '3rem' }}>
+          ¡Bienveni@ a ePeriph!
+        </Typography>
+        <Box sx={{ textAlign: 'justify', width: '50vw' }}>
+          Tu destino en línea para todo lo relacionado con periféricos de ordenador. En ePeriph, nos apasiona proporcionarte una experiencia de compra sin complicaciones y una amplia selección de
+          productos de alta calidad. Nuestro equipo de expertos en tecnología ha seleccionado cuidadosamente cada producto en nuestro catálogo para garantizar su rendimiento, durabilidad y valor
+          excepcional. Con precios competitivos y un servicio al cliente excepcional, en ePeriph estamos comprometidos a hacer que tu experiencia de compra sea impecable
+        </Box>
         <Grid>
-          <h3>Artículos destacados</h3>
+          <Typography variant="h4" textAlign="center">
+            Artículos destacados
+          </Typography>
           {products.map((product) => (
             <Article key={`product-${product.id}`} product={product} />
           ))}
